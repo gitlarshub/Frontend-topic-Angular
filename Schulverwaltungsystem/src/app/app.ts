6d6ccbe1fa21c11fa85db0ef3fe97573d7cb8729
Lars <lars.veljaca1@icloud.com>
2026-03-13 10:16:22 +0100

    Change Name of the title

--- Schulverwaltungsystem/src/app/app.ts
+++ Schulverwaltungsystem/src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('HitCounterAngular');
+  protected readonly title = signal('Schulverwaltungsystem');
 }
